bda433f50970e9e59666832b5f0f72462ba8f160
Student <student@MacBook-Pro-5.local>
2019-05-30 14:54:49 +0100

    Ch 10

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,8 +1,12 @@
-require './lib/Bike.rb'
+require_relative 'Bike'
+
 class DockingStation
 
   def release_bike
      Bike.new
   end
 
+  def bike
+
+  end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,4 +4,5 @@ require 'Bike.rb'
 
 describe DockingStation do
    it { should respond_to :release_bike}
+   it { should respond_to :dock_bike}
 end
